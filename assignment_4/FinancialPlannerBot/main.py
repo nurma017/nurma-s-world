b163a520604ccfc52ef98c0fb839ac2fffed6f92
Nurmukhammed Ziyada <ziyadanurmukhammed@MacBook-Pro-Nurmukhammed-2.local>
2025-04-15 20:42:04 +0500

    .

--- assignment_4/FinancialPlannerBot/main.py
+++ assignment_4/FinancialPlannerBot/main.py
@@ -1,17 +1,9 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from handlers import start, help_command, config, log, summary, notify_on, notify_off
+from telegram.ext import Application, CommandHandler
+from handlers import start  
 from config import TOKEN
 
 app = Application.builder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("config", config))
-app.add_handler(CommandHandler("log", log))
-app.add_handler(CommandHandler("summary", summary))
-app.add_handler(CommandHandler("notifyon", notify_on))
-app.add_handler(CommandHandler("notifyoff", notify_off))
 
-if __name__ == '__main__':
-    app.run_polling()
+
+app.run_polling()
